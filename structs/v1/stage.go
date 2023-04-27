588617bbec0fc22c7514d30544c45e13af53deff
Jannek Rodenburger <jannek.rodenburger@vrg.de>
2023-04-27 14:42:54 +0200

    added regex to environment list

--- structs/v1/stage.go
+++ structs/v1/stage.go
@@ -3,7 +3,8 @@ package v1
 type EnvironmentList map[string]Environment
 
 type Environment struct {
-	Name         string `json:"name"`
-	ClusterAPI   string `json:"clusterapi"`
-	AppendSuffix bool   `json:"appendsuffix"`
+	Name              string `json:"name"`
+	ClusterAPI        string `json:"clusterapi"`
+	AppendSuffix      bool   `json:"appendsuffix"`
+	UseRegexSubdomain bool   `json:"useregexsubdomain"`
 }
